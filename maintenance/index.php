9844c22840ce942653cb7b902d2a89863a9df957
Hiro1120 <tnero1120@gmail.com>
2023-04-24 19:53:14 +0900

    デザインの修正

--- maintenance/index.php
+++ maintenance/index.php
@@ -1,5 +1,4 @@
 <?php
-echo (password_hash("12345", PASSWORD_BCRYPT));
 $pathFile = file(__DIR__ . '/path.txt');
 $path = explode('：',$pathFile[0]);
 //パスワード（暗号化）
@@ -7,47 +6,42 @@ $path = explode('：',$pathFile[0]);
 ?>
 
 <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-        <!--キャッシュを無効にする-->
-        <meta http-equiv="Cache-Control" content="no-store">
+<html>
+<head>
+    <!--キャッシュを無効にする-->
+    <meta http-equiv="Cache-Control" content="no-store">
 
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/top.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/top.css">
 
-        <title>ようこそ</title>
-    </head>
-    <body>
-        <div class="btn_block">
-            <div class="btn-border-gradient-wrap btn-border-gradient-wrap--gold">
-                <a href="../top.php" class="btn btn-border-gradient"><span class="btn-text-gradient--gold">つぶやきの世界へ</span></a>
-            </div>
-        </div>
-        <table>
-            <tr>
-                <td>
-                    <div class="image_block">
-                        <img src="../image/icon2.png">
-                    </div>
-                </td>
-                <td>
-                    <div class="your_message">
-                        <div class="heading06">
-                            <h2 class="heading06" data-en="言葉が世界をつなぎ、あなたが世界を変える"><font weight="bold"><span class="btn-text-gradient--gold">世界中へあなたの言葉を届けよう！！</span></font></h2>
-                        </div>
-                        <h4><span class="btn-text-gradient--color2">つぶやきで世界を変えろ。</span></h4>
-                        <h4><span class="btn-text-gradient--color2">　　　　　　つぶやきで未来を作れ。</span></h4>
-                        <br>
-                        <br>
-                        <h3><span class="btn-text-gradient--color">つぶやきの世界を体験しよう！</span></h3>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </body>
+	<title>ようこそ</title>
+	<link rel="stylesheet" type="text/css" href="../css/top.css">
+</head>
+<body>
+	<header>
+		<h1>PHP Twitter</h1>
+	</header>
+	<section>
+		<h2>アプリの説明</h2>
+		<p>PHP/HTML/CSSのみで実装したアプリです。</p>
+	</section>
+	<section>
+		<h2>アプリの特徴</h2>
+		<ul>
+			<li>アカウントが作成できる</li>
+			<li>ログイン機能がある</li>
+			<li>ツイートができる</li>
+		</ul>
+	</section>
+	<section>
+		<h2>ご利用いただくには</h2>
+		<p>アカウント作成後、アプリにログインすることで本アプリを利用できます。</p>
+	</section>
+	<button class="app-btn" onclick="location.href='../top.php'">アプリを開く</button>
+</body>
 </html>
